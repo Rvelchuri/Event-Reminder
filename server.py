4de71dd1037c52404bbaa5e881009cb93a96d072
Rajani <rajani.velchuri@gmail.com>
2021-11-14 09:16:27 -0600

    updated with Twilio

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ app.config['MAIL_SERVER'] = 'smtp.sendgrid.net'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
-app.config['MAIL_PASSWORD'] = "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"
+app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = "rajani.velchuri@gmail.com"
 mail = Mail(app)
 
@@ -254,9 +254,10 @@ def get_upcoming_wedding_day():
 @app.route('/sendmail', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        pdb.set_trace()
+        # pdb.set_trace()
         print (request.data)
         recipient = request.json['recipient']
+        type = request.json["type"]
         msg = Message('Twilio SendGrid Test Email', recipients=[recipient])
         msg.body = ('Congratulations! You have sent a test email with '
                     'Twilio SendGrid!')
